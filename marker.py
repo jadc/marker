4e48019ca94898238fadd19b3a4056dc6f511352
jadc <chehimj@gmail.com>
2024-02-03 20:36:17 -0700

    scripts now must traverse to directory, not marker

--- marker.py
+++ marker.py
@@ -12,8 +12,8 @@ def run(argv, cwd=None):
     logging.debug(f"Running '{cmd_as_str}' in {cwd}")
     cmd = subprocess.run(argv, cwd=cwd, capture_output=True, text=True)
     if(cmd.stdout): logging.debug(cmd.stdout)
-    if(cmd.returncode): logging.error(f"Failed to run '{cmd_as_str}': {cmd.stderr}")
-    return cmd.returncode
+    if(cmd.returncode): logging.error(f"Failed to run '{cmd_as_str}' ({cmd.returncode}): {cmd.stderr}")
+    return cmd
 
 async def grade(ccid: str, repo: str, script_file: str):
     async with sem:
@@ -21,21 +21,15 @@ async def grade(ccid: str, repo: str, script_file: str):
         with tempfile.TemporaryDirectory() as d:
             logging.debug(f"Created temporary directory, '{d}'")
 
-            run(["git", "clone", repo, "."], cwd=d) 
-            run(["cp", script_file, d])
-            run(["chmod", "+x", script_file], cwd=d)
+            # Clone student repository
+            cmd = run( ["git", "clone", repo, Path(d)] )
+            if(cmd.returncode):
+                return (ccid, f"ERROR ({cmd.returncode}) SEE LOG")
 
-            # Run marking script
-            cmd = subprocess.run(["./" + script_file, "."], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=d)
-
-            if(cmd.stdout): 
-                logging.debug(f"Marking script stdout: {cmd.stdout}")
-            if(cmd.returncode): 
-                logging.error(f"Failed to run marking script (code {cmd.returncode})")
-                return (ccid, f"ERROR: {cmd.returncode}")
-            if(cmd.stderr): 
-                logging.error(f"Failed to run marking script\n{cmd.stderr}")
-                return (ccid, f"ERROR: {cmd.stderr}")
+            # Run marking script in current directory, giving it submission directory
+            cmd = run( ["./" + script_file, Path(d)] )
+            if(cmd.returncode):
+                return (ccid, f"ERROR ({cmd.returncode}) SEE LOG")
 
             output = cmd.stdout.strip().split("\n")
             grade = output.pop().split("/")[0]
@@ -59,7 +53,7 @@ async def gather(csv_file: str, script_file: str, out_file: str):
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
     p.add_argument("csv", type=str, help="submissions csv (from 'Download grades' on GitHub Classroom)")
-    p.add_argument("script", type=str, help="any script (with a shebang), run in root of student repo")
+    p.add_argument("script", type=str, help="any script (with a shebang) with first argument being directory to grade")
     p.add_argument("-o", "--output", type=str, default="grades.csv", help="output csv including CCID and corresponding lab grade")
     p.add_argument("-v", "--verbose", action="store_true")
     args = p.parse_args()
