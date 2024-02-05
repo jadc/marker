ac6ea90fc035c90bf0b048d8938898ad0e5cd1d7
jadc <chehimj@gmail.com>
2024-02-04 20:48:23 -0700

    feedback as github issues

--- marker.py
+++ marker.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 # marker by jadc
+MAX_POINTS = 10
 
-import logging, argparse, csv, asyncio, tempfile, subprocess
+import re, logging, argparse, csv, asyncio, tempfile, subprocess
 from pathlib import Path
-from datetime import datetime
 
 sem = asyncio.Semaphore(1)
 
 def run(argv, cwd=None):
     logging.debug(f"Running {argv} in {cwd}")
     cmd = subprocess.run(argv, cwd=cwd, capture_output=True, text=True)
-    if(cmd.stdout): logging.debug(cmd.stdout)
+    if(cmd.stdout):     logging.debug(cmd.stdout)
     if(cmd.returncode): logging.error(f"CODE {cmd.returncode} WITH {argv}\n{cmd.stderr}")
     return cmd
 
-async def grade(ccid: str, repo: str, script_file: str):
+async def grade(ccid: str, repo: str, script_file: str, publish: bool):
     async with sem:
         logging.info(f"Grading {ccid}...")
         with tempfile.TemporaryDirectory() as d:
@@ -29,18 +29,29 @@ async def grade(ccid: str, repo: str, script_file: str):
             cmd = run( ["./" + script_file, Path(d)] )
             if(cmd.returncode):
                 return (ccid, f"ERROR ({cmd.returncode}) SEE LOG")
+            
+            if publish: return (ccid, extract_mark(cmd.stdout), feedback(repo, cmd.stdout))
+            else:       return (ccid, extract_mark(cmd.stdout), "Run with '--publish' for feedback")
 
-            output = cmd.stdout.strip().split("\n")
-            grade = output.pop().split("/")[0]
+def extract_mark(stdout: str):
+    r = re.search(r"Total: (\d+)/(\d+)", stdout)
+    return round((float(r.group(1)) / float(r.group(2)))*MAX_POINTS, 2)
 
-            return (ccid, grade, ". ".join(output))
+# Creates issue on GitHub repo containing feedback
+# Returns URL to issue
+def feedback(repo: str, stdout: str):
+    args = ["gh", "issue", "create", "--title", "Feedback", "-R", repo, "-F", "-"]
+    cmd = subprocess.run(args, stdout=subprocess.PIPE, input=stdout, text=True)
+    url = cmd.stdout.strip().replace("https://", "")
+    logging.debug(f"Published feedback to '{url}'")
+    return url
 
 # Read CSV for CCIDs and GitHub repositories
-async def gather(csv_file: str, script_file: str, out_file: str):
+async def gather(csv_file: str, script_file: str, out_file: str, publish: bool):
     with open(Path(csv_file), "r", newline="", encoding="utf-8") as f:
         reader = csv.reader(f)
         next(reader)  # skip headings
-        submissions = [ grade(x[4], x[6], script_file) for x in reader if x[4] and x[6] ]
+        submissions = [ grade(x[4], x[6], script_file, publish) for x in reader if x[4] and x[6] ]
     results = await asyncio.gather(*submissions)
 
     with open(Path(out_file), "w", newline="", encoding="utf-8") as f:
@@ -54,6 +65,7 @@ if __name__ == "__main__":
     p.add_argument("csv", type=str, help="submissions csv (from 'Download grades' on GitHub Classroom)")
     p.add_argument("script", type=str, help="any script (with a shebang) with first argument being directory to grade")
     p.add_argument("-o", "--output", type=str, default="grades.csv", help="output csv including CCID and corresponding lab grade")
+    p.add_argument("--publish", action="store_true", help="publishes feedback to students repos; only use after testing!")
     p.add_argument("-v", "--verbose", action="store_true")
     args = p.parse_args()
 
@@ -61,4 +73,5 @@ if __name__ == "__main__":
     if(args.verbose): l = logging.DEBUG
     logging.basicConfig(format='%(asctime)s %(levelname)s | %(message)s', level=l, datefmt='%Y-%m-%d %H:%M:%S')
 
-    asyncio.run(gather(args.csv, args.script, args.output))
+    if(args.publish): logging.info("PUBLISH MODE!!!")
+    asyncio.run(gather(args.csv, args.script, args.output, args.publish))
